70677ef6780dfe1b0369ed2b4c23ef410c175ba2
Yves Hoppe <yves@compojoom.com>
2016-04-15 16:17:45 +0200

    Updated copyright headers and jorobo.dist.ini to include space before the asterisk

--- src/Tasks/Build/Language.php
+++ src/Tasks/Build/Language.php
@@ -1,10 +1,10 @@
 <?php
 /**
-* @package     JoRobo
-*
-* @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
-* @license     GNU General Public License version 2 or later; see LICENSE.txt
-*/
+ * @package     JoRobo
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 namespace Joomla\Jorobo\Tasks\Build;
 
 use Robo\Result;
