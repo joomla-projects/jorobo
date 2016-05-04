3b4e943b311db967692a6a422ef82d61b79bcf8c
Abu-Huraira <shamsbd71@users.noreply.github.com>
2016-05-04 20:03:40 +0600

    Updated Package.php to add media folder to zip

--- src/Tasks/Deploy/Package.php
+++ src/Tasks/Deploy/Package.php
@@ -221,6 +221,7 @@ class Package extends Base implements TaskInterface
 		$this->_remove(JPATH_BASE . '/dist/tmp/cbuild/administrator/manifests');
 		$this->_copyDir($this->current . '/language', JPATH_BASE . '/dist/tmp/cbuild/language');
 		$this->_copyDir($this->current . '/components', JPATH_BASE . '/dist/tmp/cbuild/components');
+		$this->_copyDir($this->current . '/media', JPATH_BASE . '/dist/tmp/cbuild/media');
 
 		$comZip->open(JPATH_BASE . '/dist/zips/com_' . $this->getExtensionName() . '.zip', \ZipArchive::CREATE);
 
