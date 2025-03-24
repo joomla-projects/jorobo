654419b1db72eafff3c90361c7e1aed4c876de12
Hannes Papenberg <info@joomlager.de>
2025-03-24 21:47:13 +0100

    Adding JoRobo init script

--- docs/Setup.md
+++ docs/Setup.md
@@ -1,3 +1,48 @@
+## Setup of your repository
+JoRobo allows to host all custom extensions for a project or all extensions for an extension pakage in one repository. The goal is to easily commit all changes to your code to a versioning system, to build packages automatically from this source and to ensure constant quality of the code.
+
+### Quick-Setup
+In an empty repository, run the following commands to create a composer.json, add JoRobo as a dependency and then initialise the repository with the selected features:
+```bash
+composer init
+composer require --dev joomla-projects/jorobo
+vendor/bin/jorobo init
+```
+
+### Folder structure
+The advised structure is to have a git repository with the following files and folders:
+```
+root
+├─ docs
+├─ src
+└─ tests
+```
+An example for such a repository would be the [Weblinks](https://github.com/joomla-extensions/weblinks) repository or this repository itself.
+While the names should be self-explanatory, the `src` folder holds your source code in a structure so that you can simply copy this over a Joomla installation and everything should be in the right folder.
+
+This means that you would for example have the following structure:
+```
+root
+└─ src
+  ├─ administrator
+  │  └─ components
+  │     └─ com_example
+  │        ├─ ...
+  │        └─ ...
+  ├─ components
+  │  └─ com_example
+  │     ├─ ...
+  │     └─ ...
+  └─ modules
+     └─ mod_example
+        ├─ ...
+        └─ ...
+```
+
+### Initialising JoRobo in the repo
+You will need `composer`. In the folder of your repository run `composer require --dev joomla-projects/jorobo` to install both JoRobo and Robo.li. To run JoRobo tasks, you now need a `RoboFile.php` in the root of your repository. As a start, you can copy a default file from `/vendor/joomla-project/jorobo/assets/`. You also have to create a `jorobo.ini` in the root. An example for this can also be found in the mentioned assets folder. Alternatively JoRobo provides an init script to prepare your repository automatically. Simply call `vendor/bin/jorobo init`.
+
+
 ## How-to use in your own extension
 
 Do a composer require joomla-projects/jorobo:dev
@@ -33,30 +78,30 @@ In order to use JoRobo you should use the following directory structure (it's li
 #### Components
 
 ```
-source/administrator/components/com_name/
-source/administrator/components/com_name/name.xml
-source/administrator/components/com_name/script.php (Optional)
-source/components/com_name/
-source/administrator/language/en-GB/en-GB.com_name.ini
-source/administrator/language/en-GB/en-GB.com_name.sys.ini
-source/language/en-GB/en-GB.com_name.ini
-source/media/com_name
+src/administrator/components/com_name/
+src/administrator/components/com_name/name.xml
+src/administrator/components/com_name/script.php (Optional)
+src/components/com_name/
+src/administrator/language/en-GB/en-GB.com_name.ini
+src/administrator/language/en-GB/en-GB.com_name.sys.ini
+src/language/en-GB/en-GB.com_name.ini
+src/media/com_name
 ```
 
 #### Modules
 
 ```
-source/modules/mod_something
-source/media/mod_something
-source/language/en-GB/en-GB.mod_something.ini
+src/modules/mod_something
+src/media/mod_something
+src/language/en-GB/en-GB.mod_something.ini
 ```
 
 #### Plugins
 
 ```
-source/plugins/type/name
-source/media/plg_type_name
-source/administrator/language/en-GB/en-GB.plg_type_name.ini
+src/plugins/type/name
+src/media/plg_type_name
+src/administrator/language/en-GB/en-GB.plg_type_name.ini
 ```
 
 ### Extension setup
---
+++ src/Command/InitCommand.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * @package    JoRobo
+ *
+ * @copyright  Copyright (C) 2005 - 2025 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Jorobo\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Console command to initialise a repo for JoRobo
+ *
+ * @since  1.0
+ */
+class InitCommand extends Command
+{
+    /**
+     * Configure the command.
+     *
+     * @return  void
+     *
+     * @since   1.0.0
+     */
+    protected function configure(): void
+    {
+        $this->setName('init')
+            ->setDescription('Initialise a folder as a Joomla extension repository.')
+        ;
+    }
+
+    /**
+     * Internal function to execute the command.
+     *
+     * @param   InputInterface   $input   The input to inject into the command.
+     * @param   OutputInterface  $output  The output to inject into the command.
+     *
+     * @return  integer  The command exit code
+     *
+     * @since   1.0.0
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('JoRobo Init');
+        $io->info('Initialising folder for a Joomla extension');
+
+        if (is_dir(JPATH_ROOT) && !is_file(JPATH_ROOT . '/composer.json')) {
+            $io->error('The script is run from an unknown place and can\'t reliably find the root path of the repository. The discovered path was ' . JPATH_ROOT);
+
+            return Command::FAILURE;
+        }
+
+        // Do we initialise with all features?
+        $all = $io->ask('Do you want to initialise with all options? (JoRobo, gitignore, codestyle, phpstan, tests)', 'yes');
+
+        if (!is_dir(JPATH_ROOT . '/src')) {
+            $io->writeln('Creating /src folder');
+            mkdir(JPATH_ROOT . '/src');
+        }
+
+        $io->writeln('Setting up JoRobo');
+        $this->copy(JOROBO_ROOT . '/assets/init/RoboFile.php', JPATH_ROOT . '/RoboFile.php');
+        $this->copy(JOROBO_ROOT . '/assets/init/jorobo.dist.ini', JPATH_ROOT . '/jorobo.dist.ini');
+
+        if ($all == 'yes' || $io->ask('Want to add codestyle checks?', 'yes') === 'yes') {
+            $io->writeln('Setting up codestyle checks');
+            $this->copy(JOROBO_ROOT . '/assets/init/.editorconfig', JPATH_ROOT . '/.editorconfig');
+            $this->copy(JOROBO_ROOT . '/assets/init/.php-cs-fixer.dist.php', JPATH_ROOT . '/.php-cs-dist-fixer.php');
+            $this->copy(JOROBO_ROOT . '/assets/init/ruleset.xml', JPATH_ROOT . '/ruleset.xml');
+
+            exec('cd ' . JPATH_ROOT . ' && composer require --dev squizlabs/php_codesniffer friendsofphp/php-cs-fixer');
+        }
+
+        if ($all == 'yes' || $io->ask('Want to add gitignore file?', 'yes') === 'yes') {
+            $io->writeln('Setting up gitignore');
+            $this->copy(JOROBO_ROOT . '/assets/init/.gitignore', JPATH_ROOT . '/.gitignore');
+        }
+
+        if ($all == 'yes' || $io->ask('Want to add phpstan static code analysis?', 'yes') === 'yes') {
+            $io->writeln('Setting up phpstan');
+            $this->copy(JOROBO_ROOT . '/assets/init/phpstan.neon', JPATH_ROOT . '/phpstan.neon');
+
+            exec('cd ' . JPATH_ROOT . ' && composer require --dev phpstan/phpstan phpstan/phpstan-deprecation-rules');
+        }
+
+        if ($all == 'yes' || $io->ask('Want to add phpunit tests?', 'yes') === 'yes') {
+            $io->writeln('Setting up phpunit');
+            $this->copy(JOROBO_ROOT . '/assets/init/phpunit.xml.dist', JPATH_ROOT . '/phpunit.xml.dist');
+
+            exec('cd ' . JPATH_ROOT . ' && composer require --dev phpunit/phpunit');
+        }
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * Helper function to cleanup paths before copying files
+     *
+     * @param   string  $src Source file to copy from
+     * @param   string  $dst Destination file to copy to
+     *
+     * @return bool
+     */
+    private function copy($src, $dst)
+    {
+        if ('\\' === \DIRECTORY_SEPARATOR) {
+            $src = strtr($src, '/', '\\');
+            $dst = strtr($dst, '/', '\\');
+        }
+
+        return copy($src, $dst);
+    }
+}
