80c673a6d160aa915f8ed98ef12e4d9145f2b3fe
Moritz Kaufmann <55949980+CraftException@users.noreply.github.com>
2026-07-13 14:25:13 +0200

    Used full plugin name for file destination in Language.php

--- src/Tasks/Build/Language.php
+++ src/Tasks/Build/Language.php
@@ -78,7 +78,7 @@ class Language extends Base
             $dest .= "/modules/" . $this->ext;
         } elseif ($this->type == "plg") {
             $a    = explode("_", $this->ext);
-            $dest .= "/plugins/" . $a[1] . "/" . $a[2];
+            $dest .= "/plugins/" . $a[1] . "/" . array_slice($a, 2);
         } elseif ($this->type == "pkg") {
             $dest .= "/administrator/manifests/packages/" . $this->ext;
         } elseif ($this->type == "lib") {
